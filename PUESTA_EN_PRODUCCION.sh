0a0cf1952d4a6d2522d225d0de771e1ec1a5c13c
Javier <jsm88_live@hotmail.com>
2016-01-27 16:59:30 +0100

    Update PUESTA_EN_PRODUCCION.sh

--- PUESTA_EN_PRODUCCION.sh
+++ PUESTA_EN_PRODUCCION.sh
@@ -2,7 +2,6 @@
 #Utilizar la contraseña B.lack14 y para la SSH Key "despliegue"
 #Este comando ejecuta un proceso ssh para conectar a la ip indicada con un usuario, y una vez logeado se ejectutan las siguientes instrucciones.
 git add --all && git commit -m "$1" && git push origin master
-
 ssh  jscmdesp@185.42.104.219 -t " 
 cd /web/produccion/ &&
 sh ../Server_PUESTA_EN_PRODUCCION.sh; bash"
