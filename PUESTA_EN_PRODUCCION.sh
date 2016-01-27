152c972f7651f2b99aee08a452a20c30f8e509bc
Javier <jsm88_live@hotmail.com>
2016-01-27 17:01:04 +0100

    Update PUESTA_EN_PRODUCCION.sh

--- PUESTA_EN_PRODUCCION.sh
+++ PUESTA_EN_PRODUCCION.sh
@@ -3,6 +3,7 @@
 #Este comando ejecuta un proceso ssh para conectar a la ip indicada con un usuario, y una vez logeado se ejectutan las siguientes instrucciones.
 git add --all && git commit -m "$1" && git push origin master
 
+
 ssh  jscmdesp@185.42.104.219 -t " 
 cd /web/produccion/ &&
 sh ../Server_PUESTA_EN_PRODUCCION.sh; bash"
