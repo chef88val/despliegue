d1747f737c68fb93700c0fd0ab38a965982913fa
Javier <jsm88_live@hotmail.com>
2016-01-27 16:55:19 +0100

    Update PUESTA_EN_PRODUCCION.sh

--- PUESTA_EN_PRODUCCION.sh
+++ PUESTA_EN_PRODUCCION.sh
@@ -1,8 +1,7 @@
 #El usuario CDmon es js_cmd y la contraseña B.lack14
 #Utilizar la contraseña B.lack14 y para la SSH Key "despliegue"
 #Este comando ejecuta un proceso ssh para conectar a la ip indicada con un usuario, y una vez logeado se ejectutan las siguientes instrucciones.
-git add --all &&
-git commit -m "$1" && git push origin master
+git add --all && git commit -m "$1" && git push origin master
 
 
 ssh  jscmdesp@185.42.104.219 -t " 
